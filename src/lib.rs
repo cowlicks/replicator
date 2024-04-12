4705222031fb0d3c53a52dc26078d8f22cbb8a12
Blake Griffith <blake.a.griffith@gmail.com>
2024-04-12 14:36:14 -0500

    initial commit Replicator Hypercore impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,59 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+/// trait for replication
+/// unfortunately this thing has to take `self` because it usually consumes the thing
+use futures_lite::{AsyncRead, AsyncWrite, Future, StreamExt};
+use std::{fmt::Debug, marker::Unpin};
+use thiserror::Error;
+use tracing::info;
+
+use random_access_storage::RandomAccess;
+
+use hypercore::Hypercore;
+use hypercore_protocol::{Duplex, Event, ProtocolBuilder};
+
+#[derive(Error, Debug)]
+#[non_exhaustive]
+pub enum ReplicatorError {
+    #[error("There was an error in the opening the protocol in handshake")]
+    // TODO add key and show it
+    HypercoreError(#[from] std::io::Error),
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub trait Replicator {
+    fn replicate<S>(
+        self,
+        stream: S,
+        is_initiator: bool,
+    ) -> impl Future<Output = Result<(), ReplicatorError>> + Send
+    where
+        S: AsyncRead + AsyncWrite + Send + Unpin + 'static;
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl<T: RandomAccess + Debug + Send> Replicator for Hypercore<T> {
+    fn replicate<S>(
+        self,
+        stream: S,
+        is_initiator: bool,
+    ) -> impl Future<Output = Result<(), ReplicatorError>> + Send
+    where
+        S: AsyncRead + AsyncWrite + Send + Unpin + 'static,
+    {
+        let key = self.key_pair().public.to_bytes().clone();
+        async_std::task::spawn(async move {
+            let mut protocol = ProtocolBuilder::new(is_initiator).connect(stream);
+            while let Some(event) = protocol.next().await {
+                let event = event.unwrap();
+                info!("protocol event {:?}", event);
+                match event {
+                    Event::Handshake(_m) => {
+                        info!("got handshake! {_m:?}");
+                        if is_initiator {
+                            protocol.open(key).await?;
+                        }
+                    }
+                    _ => todo!(),
+                }
+            }
+            Ok(())
+        })
     }
 }
