f25b6fa45f54bb3805aab797a08558c5c76c18cd
Blake Griffith <blake.a.griffith@gmail.com>
2024-04-22 15:11:27 -0500

    Add macros module with proc_macros

---
+++ macros/src/lib.rs
@@ -0,0 +1,15 @@
+use proc_macro::*;
+use syn;
+
+#[proc_macro_attribute]
+pub fn start_func_with(code: TokenStream, input: TokenStream) -> TokenStream {
+    let mut item: syn::Item = syn::parse(input).unwrap();
+    let fn_item = match &mut item {
+        syn::Item::Fn(fn_item) => fn_item,
+        _ => panic!("expected fn"),
+    };
+    fn_item.block.stmts.insert(0, syn::parse(code).unwrap());
+
+    use quote::ToTokens;
+    item.into_token_stream().into()
+}
