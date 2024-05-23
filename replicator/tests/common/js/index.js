96d17a8e01968e8d668de0c2e6a9fab21bb5d8c9
Blake Griffith <blake.a.griffith@gmail.com>
2024-05-23 15:32:05 -0400

    clean up js tests

--- replicator/tests/common/js/index.js
+++ replicator/tests/common/js/index.js
@@ -14,75 +14,69 @@ const logBlue = f => {
   process.stdout.write('\x1b[0m');
   return out;
 }
-const l = (...x) => [logBlue(() => console.log(...x)), x[0]][1];
+const l = (...x) => [console.log(...x), x[0]][1];
 
 const sleep = (ms) => new Promise(r => setTimeout(r, ms));
+const pause = () => sleep(500);
 
-const cmpWithRs = (async () => {
-  const a = new Hypercore(RAM, undefined, {name: 'Writer', log: (...x)  => logYellow(() => console.log(...x))});
-  await a.ready();
-  l('--A ready');
-  await a.append(['a', 'b', 'c']);
-
-  const b = new Hypercore(RAM, a.key, {name: 'Reader'});
-  await b.ready();
-  l('--B ready');
-
-  const s1 = a.replicate(true, { keepAlive: false });
-  l('--A replication stream created')
-  const s2 = b.replicate(false, { keepAlive: false });
-  l('--B replication stream created')
-  s1.pipe(s2).pipe(s1);
-  l('--A and B piped together');
-
-  await sleep(1);
-  b.update({wait: true});
-  await sleep(1);
-
-  if ((await b.info()).length != (await a.info()).length) {
-    throw 'fail'
-  }
-  l('--------------B udate complete. Adding more data to A------------------');
-
-  await a.append(['d']);
-  await sleep(1);
-  b.update({wait: true});
-
-  if ((await b.info()).length != (await a.info()).length) {
-    throw 'fail'
-  }
-});
-
-//const doesWriterBroadcast = (async () => {
-const doesWriterBroadcast = (async () => {
-  l('--- JUST SETUP ---');
-  const a = new Hypercore(RAM, undefined, {name: 'Writer', log: (...x)  => logYellow(() => console.log(...x))});
+const noInitialData = (async () => {
+  const a = new Hypercore(RAM, undefined, {name: 'Writer'});
   await a.ready();
   const b = new Hypercore(RAM, a.key, {name: 'Reader'});
   await b.ready();
+  await pause();
+
   const s1 = a.replicate(false, { keepAlive: false });
   const s2 = b.replicate(true, { keepAlive: false });
   s1.pipe(s2).pipe(s1);
-  await sleep(1e3);
+
+  await pause();
   /* Writer and Reader both send syncs */
-  l(b.replicator.peers[0].tx_msgs);
-  //l(b.replicator.rx_msgs);
+  l('\n\nsent messages\n\n')
   l(a.replicator.peers[0].tx_msgs);
-  //l(a.replicator.rx_msgs);
-  l('----- READY --------');
-  const res = await a.append('0');
-  await sleep(1e3);
-  l('----- APPENDED --------');
-  l(await b.get(0));
-  //await sleep(1e3);
-  l('----- GOTTEN --------');
+  l(b.replicator.peers[0].tx_msgs);
+});
+
+const writerWithInitialData = (async () => {
+  const a = new Hypercore(RAM, undefined, {name: 'Writer'});
+  await a.ready();
+  const b = new Hypercore(RAM, a.key, {name: 'Reader'});
+  await b.ready();
+  await pause();
+  await a.append('0');
+  await pause();
+  const s1 = a.replicate(false, { keepAlive: false });
+  const s2 = b.replicate(true, { keepAlive: false });
+  s1.pipe(s2).pipe(s1);
+  await pause();
   /* Writer and Reader both send syncs */
-  //l(a.replicator.peers[0].tx_msgs);
-  l(a.replicator.pmsgs());
-  //l(b.replicator.rx_msgs);
-  //l(b.replicator.peers[0].tx_msgs);
-  //l(a.replicator.rx_msgs);
-  await sleep(1e3);
-})();
+  l('\n\nsent messages\n\n')
+  l(a.replicator.peers[0].tx_msgs);
+  l(b.replicator.peers[0].tx_msgs);
+});
 
+const writerNoInitialDataAddsData = (async () => {
+  const a = new Hypercore(RAM, undefined, {name: 'Writer'});
+  await a.ready();
 
+  const b = new Hypercore(RAM, a.key, {name: 'Reader'});
+  await b.ready();
+
+  const s1 = a.replicate(false, { keepAlive: false });
+  const s2 = b.replicate(true, { keepAlive: false });
+  s1.pipe(s2).pipe(s1);
+  await pause();
+  await a.append('0');
+  await pause();
+  /* Writer and Reader both send syncs */
+  l('\n\nsent messages\n\n')
+  l(a.replicator.peers[0].tx_msgs);
+  l(b.replicator.peers[0].tx_msgs);
+  await pause();
+});
+
+(async () => {
+  //await noInitialData();
+  await writerWithInitialData();
+  //await writerNoInitialDataAddsData();
+})()
